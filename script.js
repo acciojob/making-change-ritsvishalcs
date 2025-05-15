f33e4f1fd32b9a40f8fdfb44c9eee643880cc30a
Vishal Kumar <98923664+ritsvishalcs@users.noreply.github.com>
2025-05-15 14:27:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const makeChange = (c) => {
   // your name here
+	let result = {
+        q: 0,
+        d: 0,
+        n: 0,
+        p: 0
+    }
+    result.q = Math.floor(amount / 25);
+    amount %=25
+
+    result.d = Math.floor(amount / 10);
+    amount %=10
+    
+    result.n = Math.floor(amount / 5);
+    amount %=5
+
+    result.p = amount;
+    return result;
+    
 };
 
 // Do not the change the code below
